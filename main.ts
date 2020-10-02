b645e983739156d14cc6f0da7f5e95d7c6367335
Daniel <daniel.bencz@gmail.com>
2020-10-02 15:40:03 +0200

    export-import

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@
 //https://www.youtube.com/playlist?list=PLEsfXFp6DpzQMickZgPq0Pn2uQD77UXoi
 //https://github.com/codingforentrepreneurs/getting-started-with-typescript
 
+import {hello} from './hello'
+
 class SweetSweetClass {
     constructor() { 
         console.log("Even sweeter")
     }
 }
 let basil = new SweetSweetClass()
+hello()
