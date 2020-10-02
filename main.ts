8a505258261d261629c0dd8cb88caf94040d49a0
Daniel <daniel.bencz@gmail.com>
2020-10-02 12:42:45 +0200

    start project

---
+++ main.ts
@@ -0,0 +1,9 @@
+//https://www.codingforentrepreneurs.com/blog/typescript-setup-guide
+//https://www.youtube.com/playlist?list=PLEsfXFp6DpzQMickZgPq0Pn2uQD77UXoi
+
+class SweetSweetClass {
+    constructor() { 
+        console.log("Even sweeter")
+    }
+}
+let basil = new SweetSweetClass()
