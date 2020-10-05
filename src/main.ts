ee127457c7c510e5fd18db43f797832600277050
Daniel <daniel.bencz@gmail.com>
2020-10-05 09:27:49 +0200

    and it works

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@
 //https://www.youtube.com/playlist?list=PLEsfXFp6DpzQMickZgPq0Pn2uQD77UXoi
 //https://github.com/codingforentrepreneurs/getting-started-with-typescript
 
+import * as $ from "jquery";
 import { hello } from './hello'
 import { foo } from './foo'
 
 class SweetSweetClass {
 	constructor() {
+		let bar = 'bar'
 		console.log("Even sweeter")
+		$("body").css('background', 'aqua')
+		$('body').html(`<h1>${bar}</h1>`)
 	}
 }
 let basil = new SweetSweetClass()
